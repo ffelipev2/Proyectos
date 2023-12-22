ef3efcdb6d34a01b737359878b0f8a7a17de930a
ffelipev2 <ffelipev2@gmail.com>
2023-12-22 00:00:22 -0300

    actua

--- Arduino/ArduinoFlask/main.py
+++ Arduino/ArduinoFlask/main.py
@@ -1,31 +1,34 @@
-from flask import Flask, render_template, Response, request
-import serial
-import time
-serialcom = serial.Serial('COM3', 9600)
-serialcom.timeout = 1
+from flask import Flask,render_template
+from flask_socketio import SocketIO
+from flask_serial import Serial
+from eventlet import monkey_patch
+
+monkey_patch()
+
 app = Flask(__name__)
-def ledOn():
-    serialcom.write(str('on').encode())
-def ledOff():
-    serialcom.write(str('off').encode())
-def disconnect():
-    serialcom.close()
+app.config['SERIAL_TIMEOUT'] = 0.2
+app.config['SERIAL_PORT'] = 'COM3'
+app.config['SERIAL_BAUDRATE'] = 9600
+app.config['SERIAL_BYTESIZE'] = 8
+app.config['SERIAL_PARITY'] = 'N'
+app.config['SERIAL_STOPBITS'] = 1
+app.config['SECRET_KEY'] = 'secret!'
 
-@app.route("/", methods=['GET', 'POST'])
+ser =Serial(app)
+socketio = SocketIO(app)
+
+@app.route('/')
 def index():
-    c = serialcom.readline()
-    if request.method == 'POST':
-        if 'on' in request.form.to_dict():
-            ledOn()
-        if 'off' in request.form.to_dict():
-            ledOff()
-        if 'dis' in request.form.to_dict():
-            disconnect()
+    return render_template('index.html')
 
-    # if request.form['off']:
-    #	print('asdfdsafsdfasf')
-    return render_template('index.html', var=c)
+@ser.on_message()
+def handle_message(medida):
+    medida = medida.decode().replace('\r','').replace('\n','')
+    parte1, parte2 = map(float, medida.split(','))
+    print(parte1)
+    print(parte2)
+    socketio.emit('data',str(parte1))
+    socketio.emit('data2', str(parte2))
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    socketio.run(app,debug =False)
--- Arduino/ArduinoFlask/static/script.js
+++ Arduino/ArduinoFlask/static/script.js
@@ -1,40 +1,57 @@
 
-            window.onload = function () {
+window.onload = function () {
 
-                let numeroPuntos = 0;
+    let numeroPuntos = 0;
 
-                let socket = io();
+    let socket = io();
 
-                socket.on('data', function (data) {
-                    console.log(data)
-                    chart.options.data[0].dataPoints.push({y: parseFloat(data)})
-                    chart.render();
+    socket.on('data', function (data) {
+        console.log(data)
+        chart.options.data[0].dataPoints.push({y: parseFloat(data)})
+        chart.render();
 
-                })
+    })
 
-                socket.on('data2', function (data2) {
-                    console.log(data2);
-                    // Actualizar el contenido de la etiqueta <p> con la parte 2
-                    actualizarParte2(parseFloat(data2));
-                });
+    socket.on('data2', function (data2) {
+        console.log(data2);
+        // Actualizar el contenido de la etiqueta <p> con la parte 2
+        actualizarParte2(parseFloat(data2));
+        //
+        // Agregar el nuevo punto al segundo conjunto de datos
+        //chart.options.data[1].dataPoints.push({y: parseFloat(data2)});
+        //chart.render();
+    });
 
-                // Función para actualizar el contenido de la etiqueta <p>
-                function actualizarParte2(valor) {
-                    document.getElementById("parte2").innerText = "Parte 2: " + valor;
-                }
+    // Función para actualizar el contenido de la etiqueta <p>
+    function actualizarParte2(valor) {
+        document.getElementById("parte2").innerText = "Parte 2: " + valor;
+    }
 
-                let chart = new CanvasJS.Chart("chartContainer", {
-                    animationEnabled: true,
-                    theme: "light2",
-                    title: {
-                        text: "TEMPERATURA"
-                    },
-                    data: [{
-                            type: "line",
-                            indexLabelFontSize: 16,
-                            dataPoints: []
-                        }]
-                });
-                chart.render();
-
+    let chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        theme: "light2",
+        title: {
+            text: "TEMPERATURA"
+        },
+        data: [
+            {
+                type: "line",
+                indexLabelFontSize: 16,
+                name: "Data 1",
+                showInLegend: true,
+                dataPoints: []
+            },
+            /*
+            {
+                type: "line",
+                indexLabelFontSize: 16,
+                name: "Data 2",
+                showInLegend: true,
+                dataPoints: []
             }
+            */
+        ]
+    });
+    chart.render();
+
+}
